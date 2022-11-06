d8e15380b39d236cd6d8c5ebcd4967f5c3efba73
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2022-11-07 00:49:05 +0530

    Done with digital clock resposive is pending

--- Digital_Clock/index.js
+++ Digital_Clock/index.js
@@ -1,1 +1,17 @@
-let date=new Date()
+function timee()
+{
+    let date=new Date();
+    let hour=date.getHours();
+    let session= hour<12?'AM':'PM';
+    let html=document.querySelector(".clock")
+    html.innerHTML=`<span class="hr">${date.getHours()}
+    </span>:<span class="min">${date.getMinutes()}</span>:
+    <span class="sec">${date.getSeconds()}</span>:
+    <span class="session">${session}</span>`
+
+    setTimeout(()=>{
+        timee()
+    },1000)
+}
+
+timee()
